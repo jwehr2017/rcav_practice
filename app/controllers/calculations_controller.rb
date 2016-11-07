b128cb2d5a8bb88d74b40ff26c8df0ca81133adb
jwehr2017 <joewehr@Joes-MacBook.local>
2016-11-07 13:18:57 -0600

    rcav practice hw

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,26 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+  def square
+
+@number = params[:id].to_i
+@number_square = (@number**2)
+  end
+  def square_root
+@number = params[:id].to_i
+@number_square_root = (@number**0.5)
+  end
+  def random
+@number_1 = params[:id_low].to_i
+@number_2 = params[:id_high].to_i
+@random = Random.new.rand(@number_1..@number_2)
+end
+def payment
+@year = params[:year].to_i
+@loan = params[:loan].to_i
+@rate = params[:rate].to_f
+@months = @year * 12
+@interest = @rate/120000
+@payment = @loan*(@interest*((1+@interest)**(@months)))/((1+@interest)**(@months)-1)
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "calculations", :action => "instructions" })
+  # Instructions Page
+  get('/', { :controller => "calculations", :action => "instructions" })
+  # Get Square
+  get('/square/:id', { :controller => "calculations", :action => "square" })
+  # Get Square Root
+  get('/square_root/:id', { :controller => "calculations", :action => "square_root" })
+  # Get Random Number
+  get('/random/:id_low/:id_high', { :controller => "calculations", :action => "random" })
+  # Get Loan Payment
+  get('/payment/:rate/:year/:loan', { :controller => "calculations", :action => "payment" })
 end
